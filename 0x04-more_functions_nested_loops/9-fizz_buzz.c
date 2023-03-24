d545b05f1f1bcf7254ca1453ef715e974695241a
Geoffrey Gakuya <jeffgakuya99@gmail.com>
2023-03-24 10:15:36 -0700

    Fizz Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,21 +1,42 @@
 #include <stdio.h>
 
-int main() {
-    int i;
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            printf("FizzBuzz\n");
-        }
-        else if (i % 3 == 0) {
-            printf("Fizz\n");
-        }
-        else if (i % 5 == 0) {
-            printf("Buzz\n");
-        }
-        else {
-            printf("%d\n", i);
-        }
-    }
-    return 0;
+/**
+ * main - Almighty Fizzbuzz, prints numbers from 1 to 100,
+ *        but any numbers which is multiple of 3, print Fizz,
+ *        and any numbers which is multiple of 5 print Buzz,
+ *        and numbers both multiple of 3 & 5,print FizzBuzz.
+ *
+ * Return: 0 if true.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 1; number <= 100; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (number % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (number % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", number);
+		}
+		if (number != 100)
+		{
+		printf(" ");
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
 
